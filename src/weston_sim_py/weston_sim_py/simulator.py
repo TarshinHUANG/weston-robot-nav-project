2c6d13b35311232b9b23329838d65323d7f271f4
Ethan-MSI-Ubuntu22 <tarshinhuang@outlook.com>
2026-02-06 00:55:27 +0800

    Lidar scan with RViz UI

--- src/weston_sim_py/weston_sim_py/simulator.py
+++ src/weston_sim_py/weston_sim_py/simulator.py
@@ -8,6 +8,7 @@ import time
 from geometry_msgs.msg import Twist, TransformStamped
 from nav_msgs.msg import Odometry
 from tf2_ros import TransformBroadcaster
+from sensor_msgs.msg import LaserScan  
 
 # quaternion
 def euler_to_quaternion(roll, pitch, yaw):
@@ -53,12 +54,15 @@ class WestonSimulator(Node):
         self.odom_pub = self.create_publisher(Odometry, '/odom', 10)
         # TF
         self.tf_broadcaster = TransformBroadcaster(self)
+        # publish lidar
+        self.scan_pub = self.create_publisher(LaserScan, '/scan', 10)
+
         # timer
         self.dt=0.1  # 0.1s refresh
         self.create_timer(self.dt, self.update_step)
 
 
-        self.get_logger().info(f"simulator.py start！map includes {len(self.map_walls)} walls and one obstacles. Publishing Odem, tf ...")
+        self.get_logger().info(f"simulator.py start！map includes {len(self.map_walls)} walls and one obstacles. Publishing Odem, tf ... Maps and Laser are ready")
 
 
 
@@ -90,8 +94,13 @@ class WestonSimulator(Node):
         # update pose
         self.pose = np.array([x,y,theta], dtype=np.float32)
 
+        current_time = self.get_clock().now()
+
         # publish Odom & TF
-        self.publish_odom_tf(self.pose, self.vel, self.get_clock().now())
+        self.publish_odom_tf(self.pose, self.vel, current_time)
+
+        # publish lidar data
+        self.publish_scan(self.pose, current_time)
 
         # # print log
         # self.get_logger().info(f"Pose: x={x:.2f}, y={y:.2f}, theta={theta:.2f}")
@@ -143,6 +152,76 @@ class WestonSimulator(Node):
         odom.twist.twist.angular.z = float(omega)
         
         self.odom_pub.publish(odom)
+
+
+    # publish lidar msg
+    def publish_scan(self, pose, current_time):
+        
+        scan = LaserScan()
+        scan.header.stamp = current_time.to_msg()
+        scan.header.frame_id = 'base_link' # lidar is fixed on robot 
+        
+        scan.angle_min = -np.pi
+        scan.angle_max = np.pi
+        scan.angle_increment = 2 * np.pi / 360 # scan by 1 degree
+        scan.time_increment = 0.0
+        scan.range_min = 0.1
+        scan.range_max = 10.0
+        
+        # calculate distance using function
+        ranges = self.get_laser_scan(pose, scan.angle_min, scan.angle_increment, 360)
+        scan.ranges = ranges.tolist()
+        
+        self.scan_pub.publish(scan)
+
+    # use numpy, got intersection between wall and laser
+    def get_laser_scan(self, pose, angle_min, angle_inc, num_readings):
+
+        x, y, theta = pose
+        
+        # generate all the scan angles, 360
+        scan_angles = angle_min + np.arange(num_readings) * angle_inc + theta
+        
+        # vector for laser (dx, dy)
+        ray_dx = np.cos(scan_angles)
+        ray_dy = np.sin(scan_angles)
+        
+        # set min distance
+        min_dists = np.full(num_readings, 10.0) # set the max range as 10m
+
+        # loop for each wall
+        for wall in self.map_walls:
+            wx1, wy1, wx2, wy2 = wall
+            
+            # vector for wall
+            wall_vec_x = wx2 - wx1
+            wall_vec_y = wy2 - wy1
+            
+            # Line-Line Intersection problem: use Cross Product          
+            denom = ray_dx * wall_vec_y - ray_dy * wall_vec_x
+            
+            # avoid parallel, which will divide by 0
+            # if denom close 0，then will be infinite large
+            valid_mask = np.abs(denom) > 1e-6
+            
+            # calculate t (distance) 和 u (intersection)
+            # vector calculation, 260 dimension
+            dx_w = wx1 - x
+            dy_w = wy1 - y
+            
+            t = (dx_w * wall_vec_y - dy_w * wall_vec_x) / denom
+            u = (dx_w * ray_dy - dy_w * ray_dx) / denom
+            
+            # valid intersection：
+            # 1. t > 0 (in the front)
+            # 2. 0 <= u <= 1 (in wall part, not extended line)
+            hit_mask = valid_mask & (t > 0) & (u >= 0) & (u <= 1)
+            
+            # upgrade min distance, if hitted
+
+            min_dists = np.where(hit_mask, np.minimum(min_dists, t), min_dists)
+
+        return min_dists
 
 
 def main(args=None):
