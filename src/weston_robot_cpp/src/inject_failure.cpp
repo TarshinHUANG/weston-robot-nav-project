c8b24d9f8439f300a0fa6b2d17be71487983de90
TarshinHUANG <15021202695@163.com>
2026-02-08 12:44:48 +0800

    Alternative way for inject_failure.cpp

--- src/weston_robot_cpp/src/inject_failure.cpp
+++ src/weston_robot_cpp/src/inject_failure.cpp
@@ -1,5 +1,12 @@
 #include "weston_robot_cpp/inject_failure.hpp"
-#include "pluginlib/class_list_macros.hpp"
+// #include "pluginlib/class_list_macros.hpp"
+#include "behaviortree_cpp_v3/bt_factory.h"
 
-// register plugin
-PLUGINLIB_EXPORT_CLASS(weston_robot_cpp::InjectFailure, BT::TreeNode)
+// // register plugin
+// PLUGINLIB_EXPORT_CLASS(weston_robot_cpp::InjectFailure, BT::TreeNode)
+
+// Alternative registration method for BehaviorTree plugins
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<weston_robot_cpp::InjectFailure>("InjectFailure");
+}
